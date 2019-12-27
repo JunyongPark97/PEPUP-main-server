80cc3942aa457c310b20e09c8b7196bf1706931d
happysmileboy <t35114105@gmail.com>
2019-12-27 12:04:14 +0900

    base user settings

--- pepup/accounts/models.py
+++ pepup/accounts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractUser, BaseUserManager, PermissionsMixin)
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError('이메일을 입력해주세요')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **kwargs):
+        kwargs.setdefault('is_staff', False)
+        kwargs.setdefault('is_superuser',False)
+        return self._create_user(email,password,**kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError('superuser must have is_staff=True')
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError('superuser must have is_superuser=True')
+        return self._create_user(email,password,**kwargs)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(verbose_name='email address', unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
