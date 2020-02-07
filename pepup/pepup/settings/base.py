fd2c15e177bcf5ace3d283ff11d46181881e3f81
JunyongPark97 <pjyong68@naver.com>
2020-02-07 16:44:04 +0900

    [FIXED] pagination size

--- pepup/pepup/settings/base.py
+++ pepup/pepup/settings/base.py
@@ -190,7 +190,7 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 51
 }
 
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
