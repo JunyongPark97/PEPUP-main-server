08dc899daf2398274c17f03aa411ab5944b9be8f
JunyongPark97 <pjyong68@naver.com>
2020-02-10 19:39:08 +0900

    [ADDED] search/searching/ api

--- pepup/api/urls.py
+++ pepup/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path,include
 from django.conf import settings
 from . import views
-from .routers import router, router_trades, router_follow, router_payment
+from .routers import router, router_trades, router_follow, router_payment, router_search
 
 app_name = 'api'
 
 urlpatterns = [
     path('pay_test/', views.pay_test),
-] + router.urls + router_trades.urls + router_payment.urls + router_follow.urls
+] + router.urls + router_trades.urls + router_payment.urls + router_follow.urls + router_search.urls
 
