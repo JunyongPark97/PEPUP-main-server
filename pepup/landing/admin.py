80212e44578876809c78ad04322b09756ce076fd
박준용 <park@bagjun-yong-ui-MacBookPro.local>
2020-02-28 22:37:42 +0900

    [UPDATED] landing succes test

--- pepup/landing/admin.py
+++ pepup/landing/admin.py
@@ -5,7 +5,7 @@ from landing.models import Register
 class RegisterAdmin(admin.ModelAdmin):
     list_display = ['name','phone','quantity', 'bank', 'account', 'created_at']
     search_fields = ('name', 'phone', 'bank', 'account')
-    ordering = ('created_at',)
+    ordering = ('-created_at',)
 
 
 admin.site.register(Register, RegisterAdmin)
