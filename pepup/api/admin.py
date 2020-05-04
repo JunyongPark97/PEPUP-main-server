d7c432e277b6b8c8145dbf0863cf9826dd9878c8
JunyongPark97 <pjyong68@naver.com>
2020-05-04 08:16:42 +0000

    [UPDATED] Admin prod image & terms-of-use

--- pepup/api/admin.py
+++ pepup/api/admin.py
@@ -1,20 +1,55 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.safestring import mark_safe
-
+from django.contrib.admin.widgets import AdminFileWidget
 from payment.models import Delivery
 from .models import Brand, Product, ProdThumbnail, Like, Tag, Follow, GenderDivision, FirstCategory, \
     SecondCategory, Size, ProdImage, ProdS3Image
-
+from django.db import models
 
 # Register your models here.
 
 
+class ProdThumbInline(admin.TabularInline):
+    model = ProdThumbnail
+    extra = 1
+    readonly_fields = ['image', 'thumbnail', 'product']
+
+    def image(self, obj):
+        if obj.thumbnail:
+            return mark_safe('<img src="%s" width=120px "/>' % obj.image_url)
+
+
+class AdminImageWidget(AdminFileWidget):
+    def render(self, name, value, attrs=None):
+        output = []
+
+        if value and getattr(value, "url", None):
+            image_url = value.url
+            file_name = str(value)
+            output.append(u' <a href="%s" target="_blank"><img src="%s" alt="%s" width="150" height="150"  style="object-fit: cover;"/></a> %s ' % \
+                (image_url, image_url, file_name, _('')))
+        output.append(super(AdminFileWidget, self).render(name, value, attrs))
+        return mark_safe(u''.join(output))
+
+
+class CollectionImageInline(admin.TabularInline):
+    formfield_overrides = {models.ImageField: {'widget': AdminImageWidget}}
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name','pk','seller','sold', 'is_refundable', 'price', 'on_discount', 'discount_rate','discounted_price', 'is_active']
+    inlines = [ProdThumbInline]
+    list_display = ['name','pk','seller','sold', 'is_refundable', 'price', 'on_discount',
+                    'discount_rate','discounted_price', 'is_active', 'prod_thumb_img']
     list_editable = ('sold', 'is_refundable', 'discount_rate', 'on_discount', 'is_active')
     fields = ('seller', 'name', 'price', 'brand', 'first_category', 'second_category', 'size', 'content',
               'tag')
+
+
+    def prod_thumb_img(self, obj):
+        prod_thumb = obj.prodthumbnail
+        if prod_thumb.thumbnail:
+            return mark_safe('<img src="%s" width=120px "/>' % prod_thumb.image_url)
 
 
 class FollowAdmin(admin.ModelAdmin):
